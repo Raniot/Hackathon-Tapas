43c7e86abc5fb1ea8345dc91ffc1433d2061d127
Nicklas <nnicklas92@gmail.com>
2019-11-22 22:04:48 +0100

    Updated webcam class

--- MyWebcam.py
+++ MyWebcam.py
@@ -2,23 +2,31 @@ from cv2 import cv2
 
 class UsbWebcam:
     def __init__(self):
+        self.vc = cv2.VideoCapture(0)
         print('Initing Webcam')
+
+    def CloseCamera(self):
+        self.vc.release()
+
+    def GetFrame(self):
+        if self.vc.isOpened(): # try to get the first frame
+            _, frame = self.vc.read()
+            return frame
 
     def GetFeed(self):
         cv2.namedWindow("preview")
-        vc = cv2.VideoCapture(0)
 
-        if vc.isOpened(): # try to get the first frame
-            rval, frame = vc.read()
+        if self.vc.isOpened(): # try to get the first frame
+            rval, frame = self.vc.read()
         else:
             rval = False
 
         while rval:
             cv2.imshow("preview", frame)
-            rval, frame = vc.read()
+            rval, frame = self.vc.read()
             key = cv2.waitKey(20)
             if key == 27: # exit on ESC
                 break
         
-        vc.release()
+        self.CloseCamera()
         cv2.destroyWindow("preview")
--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from MyWebcam import UsbWebcam
 def main():
     cam = UsbWebcam()
     
-    cam.GetFeed()
+    #cam.GetFeed() #close by press on ESC
+    
+    frame = cam.GetFrame()
+    cam.CloseCamera()
 
 
 if __name__ == "__main__":
