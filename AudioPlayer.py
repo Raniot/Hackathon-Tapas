945238b4e5c9800112f498f8ca145e978a09f472
Nicklas <nnicklas92@gmail.com>
2019-11-23 11:22:33 +0100

    Sound

--- AudioPlayer.py
+++ AudioPlayer.py
@@ -1,46 +1,40 @@
 from playsound import playsound
-from threading import Thread
+from multiprocessing import Process
 
 class AudioPlayer:
     def __init__(self):
-        self.FirstCheckpointReached = Thread()
-        self.SecondCheckpointReached = Thread()
-        self.FinalCheckpointReached = Thread()
-        self.CollissionWarning = Thread()
-        self.RouteDeviation = Thread()
-        self.SafeCross = Thread()
-        self.UnsafeCross = Thread()
         self.SoundBasePath = "./Sounds/"
+        self.FirstCheckpointReached = Process(target=self._play, args=(self.SoundBasePath + 'FirstCheckpointReached.mp3', ))
+        self.SecondCheckpointReached = Process(target=self._play, args=(self.SoundBasePath + 'SecondCheckpointReached.mp3', ))
+        self.FinalCheckpointReached = Process(target=self._play, args=(self.SoundBasePath + 'FinalCheckpointReached.mp3', ))
+        self.CollissionWarning = Process(target=self._play, args=(self.SoundBasePath + 'CollisionWarning.mp3', ))
+        self.RouteDeviation = Process(target=self._play, args=(self.SoundBasePath + 'RouteDeviation.mp3', ))
+        self.SafeCross = Process(target=self._play, args=(self.SoundBasePath + 'SafeCross.mp3', ))
+        self.UnsafeCross = Process(target=self._play, args=(self.SoundBasePath + 'UnsafeCross.mp3', ))
         self.IsPlaying = False
 
     def PlayBlocking(self, sound: str) -> None:
-        playsound(self.SoundBasePath + sound, block=True)
+        self.KillOtherSounds()
+        self._play(self.SoundBasePath + sound)
 
     def Play(self, sound: str) -> None:
         if sound == 'FirstCheckpointReached.mp3' and not self.FirstCheckpointReached.is_alive():
-            self.FirstCheckpointReached = Thread(target=self._play, args=(self.SoundBasePath + sound, ))
             self.KillOtherSounds()
             self.FirstCheckpointReached.start()
         elif sound == 'SecondCheckpointReached.mp3' and not self.SecondCheckpointReached.is_alive():
-            self.SecondCheckpointReached = Thread(target=self._play, args=(self.SoundBasePath + sound, ))
             self.KillOtherSounds()
             self.SecondCheckpointReached.start()
         elif sound == 'FinalCheckpointReached.mp3' and not self.FinalCheckpointReached.is_alive():
-            self.FinalCheckpointReached = Thread(target=self._play, args=(self.SoundBasePath + sound, ))
             self.KillOtherSounds()
             self.FinalCheckpointReached.start()
         elif sound == 'CollisionWarning.mp3' and not self.CollissionWarning.is_alive() and self.IsPlaying == False:
-            self.CollissionWarning = Thread(target=self._play, args=(self.SoundBasePath + sound, ))
             self.CollissionWarning.start()
         elif sound == 'RouteDeviation.mp3' and not self.RouteDeviation.is_alive() and self.IsPlaying == False:
-            self.RouteDeviation = Thread(target=self._play, args=(self.SoundBasePath + sound, ))
             self.RouteDeviation.start()
         elif sound == 'SafeCross.mp3' and not self.SafeCross.is_alive():
-            self.SafeCross = Thread(target=self._play, args=(self.SoundBasePath + sound, ))
             self.KillOtherSounds()
             self.SafeCross.start()
         elif sound == 'UnsafeCross.mp3' and not self.UnsafeCross.is_alive():
-            self.UnsafeCross = Thread(target=self._play, args=(self.SoundBasePath + sound, ))
             self.KillOtherSounds()
             self.UnsafeCross.start()
         else:
@@ -52,10 +46,17 @@ class AudioPlayer:
         self.IsPlaying = False
 
     def KillOtherSounds(self):
-        self.FirstCheckpointReached._stop()
-        self.SecondCheckpointReached._stop()
-        self.FinalCheckpointReached._stop()
-        self.CollissionWarning._stop()
-        self.RouteDeviation._stop()
-        self.SafeCross._stop()
-        self.UnsafeCross._stop()
+        if self.FirstCheckpointReached.is_alive():
+            self.FirstCheckpointReached.terminate()
+        if self.SecondCheckpointReached.is_alive():
+            self.SecondCheckpointReached.terminate()
+        if self.FinalCheckpointReached.is_alive():
+            self.FinalCheckpointReached.terminate()
+        if self.CollissionWarning.is_alive():
+            self.CollissionWarning.terminate()
+        if self.RouteDeviation.is_alive():
+            self.RouteDeviation.terminate()
+        if self.SafeCross.is_alive():
+            self.SafeCross.terminate()
+        if self.UnsafeCross.is_alive():
+            self.UnsafeCross.terminate()
---
+++ main2.py
@@ -0,0 +1,13 @@
+from AudioPlayer import AudioPlayer
+import time
+
+def main():
+    audio = AudioPlayer()
+    audio.Play('RouteDeviation.mp3')
+    time.sleep(2)
+    audio.KillOtherSounds()
+    # audio.PlayBlocking("SecondCheckpointReached.mp3")
+    
+
+if __name__ == "__main__":
+    main()
